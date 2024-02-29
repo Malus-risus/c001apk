c6c514d0c44ae7728cc76303af3f2cdb7f460468
Το μοχθηρό ^_^ <69906215+Malus-risus@users.noreply.github.com>
2024-02-29 19:02:16 +0800

    Update MainViewModel.kt

--- app/src/main/java/com/example/c001apk/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/c001apk/ui/main/MainViewModel.kt
@@ -1,92 +1,84 @@
-package com.example.c001apk.ui.main
+package com.example.c001apk.view
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.c001apk.adapter.Event
-import com.example.c001apk.constant.Constants
-import com.example.c001apk.logic.network.Repository.checkLoginInfo
-import com.example.c001apk.logic.network.Repository.getAppInfo
-import com.example.c001apk.util.CookieUtil
-import com.example.c001apk.util.PrefManager
-import kotlinx.coroutines.launch
-import java.net.URLEncoder
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewConfiguration
+import android.widget.FrameLayout
+import androidx.viewpager2.widget.ViewPager2
+import kotlin.math.absoluteValue
+import kotlin.math.sign
 
-class MainViewModel : ViewModel() {
+class NestedScrollableHost1 : FrameLayout {
+    private var isChildHasSameDirection = true
+    private var touchSlop = 0
+    private var initialX = 0f
+    private var initialY = 0f
 
-    var badge: Int = 0
-    var isInit: Boolean = true
-    val setBadge = MutableLiveData<Event<Boolean>>()
-
-    fun fetchAppInfo(id: String) {
-        viewModelScope.launch {
-            getAppInfo(id)
-                .collect { result ->
-                    val appInfo = result.getOrNull()
-                    if (appInfo?.data != null) {
-                        try {
-                            PrefManager.VERSION_NAME = appInfo.data.apkversionname
-                            PrefManager.API_VERSION = "13"
-                            PrefManager.VERSION_CODE = appInfo.data.apkversioncode
-                            PrefManager.USER_AGENT =
-                                "Dalvik/2.1.0 (Linux; U; Android ${PrefManager.ANDROID_VERSION}; ${PrefManager.MODEL} ${PrefManager.BUILDNUMBER}) (#Build; ${PrefManager.BRAND}; ${PrefManager.MODEL}; ${PrefManager.BUILDNUMBER}; ${PrefManager.ANDROID_VERSION}) +CoolMarket/${appInfo.data.apkversionname}-${appInfo.data.apkversioncode}-${Constants.MODE}"
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                        }
-                    }
-
-                    getCheckLoginInfo()
-                }
+    private val parentViewPager: ViewPager2?
+        get() {
+            var parent = parent
+            while (parent is View && parent !is ViewPager2) {
+                parent = parent.parent
+            }
+            return parent as ViewPager2?
         }
+
+    constructor(context: Context) : super(context) {
+        init(context)
     }
 
-    private fun getCheckLoginInfo() {
-        viewModelScope.launch {
-            checkLoginInfo()
-                .collect { result ->
-                    val response = result.getOrNull()
-                    response?.let {
-                        response.body()?.let {
-                            if (response.body()?.data?.token != null) {
-                                val login = response.body()?.data!!
-                                badge = login.notifyCount.badge
-                                CookieUtil.notification = login.notifyCount.notification
-                                CookieUtil.contacts_follow = login.notifyCount.contactsFollow
-                                CookieUtil.message = login.notifyCount.message
-                                CookieUtil.atme = login.notifyCount.atme
-                                CookieUtil.atcommentme = login.notifyCount.atcommentme
-                                CookieUtil.feedlike = login.notifyCount.feedlike
-                                CookieUtil.badge = login.notifyCount.badge
-                                PrefManager.isLogin = true
-                                PrefManager.uid = login.uid
-                                PrefManager.username = URLEncoder.encode(login.username, "UTF-8")
-                                PrefManager.token = login.token
-                                PrefManager.userAvatar = login.userAvatar
-                            } else if (response.body()?.message == "登录信息有误") {
-                                PrefManager.isLogin = false
-                                PrefManager.uid = ""
-                                PrefManager.username = ""
-                                PrefManager.token = ""
-                                PrefManager.userAvatar = ""
-                            }
-
-                            try {
-                                val headers = response.headers()
-                                val cookies = headers.values("Set-Cookie")
-                                val session = cookies[0]
-                                val sessionID = session.substring(0, session.indexOf(";"))
-                                CookieUtil.SESSID = sessionID
-                            } catch (e: Exception) {
-                                e.printStackTrace()
-                            }
-
-                            if (badge != 0)
-                                setBadge.postValue(Event(true))
-
-                        }
-                    }
-                }
-        }
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init(context)
     }
 
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(context)
+    }
+
+    private fun init(context: Context) {
+        touchSlop = ViewConfiguration.get(context).scaledTouchSlop
+    }
+
+    private fun canChildScroll(orientation: Int, delta: Float): Boolean {
+        val direction = -delta.sign.toInt()
+        var child = parent
+        while (child is View && child !is ViewPager2) {
+            child = child.parent
+        }
+
+        return (orientation == 0 && child.canScrollHorizontally(direction)) ||
+                (orientation == 1 && child.canScrollVertically(direction))
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                initialX = event.x
+                initialY = event.y
+                parent.requestDisallowInterceptTouchEvent(true)
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val dx = event.x - initialX
+                val dy = event.y - initialY
+                val isVpHorizontal = parentViewPager?.orientation == ORIENTATION_HORIZONTAL
+
+                val scaledDx = dx.absoluteValue * if (isVpHorizontal && !isChildHasSameDirection) .5f else 1f
+                val scaledDy = dy.absoluteValue * if (!isVpHorizontal && !isChildHasSameDirection) .5f else 1f
+
+                if (scaledDx > touchSlop || scaledDy > touchSlop) {
+                    if (isVpHorizontal == (scaledDy > scaledDx)) {
+                        parent.requestDisallowInterceptTouchEvent(canChildScroll(if (isVpHorizontal) 0 else 1, if (isVpHorizontal) dx else dy))
+                    } else {
+                        parent.requestDisallowInterceptTouchEvent(false)
+                    }
+                }
+            }
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> parent.requestDisallowInterceptTouchEvent(false)
+        }
+        return super.onTouchEvent(event)
+    }
 }
