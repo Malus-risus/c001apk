9f2ef3f0aabbd6b9b912f6977be5f02fa797dfe8
Το μοχθηρό ^_^ <69906215+Malus-risus@users.noreply.github.com>
2024-03-02 15:48:13 +0800

    Update AddCookiesInterceptor.kt

--- app/src/main/java/com/example/c001apk/util/AddCookiesInterceptor.kt
+++ app/src/main/java/com/example/c001apk/util/AddCookiesInterceptor.kt
@@ -7,41 +7,43 @@ import com.example.c001apk.constant.Constants.LOCALE
 import com.example.c001apk.constant.Constants.MODE
 import com.example.c001apk.constant.Constants.REQUEST_WITH
 import com.example.c001apk.util.CookieUtil.SESSID
+import com.example.c001apk.util.TokenDeviceUtils.getLastingDeviceCode
 import com.example.c001apk.util.TokenDeviceUtils.getTokenV2
 import okhttp3.Interceptor
-import okhttp3.Request
 import okhttp3.Response
-import java.io.IOException
 
 internal class AddCookiesInterceptor : Interceptor {
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
-        val builder: Request.Builder = chain.request().newBuilder()
-        val deviceCode = TokenDeviceUtils.getLastingDeviceCode()
-        val token = deviceCode.getTokenV2()
-        builder.apply {
-            addHeader("User-Agent", PrefManager.USER_AGENT)
+        val originalRequest = chain.request()
+        val deviceCode = getLastingDeviceCode()
+        val token = getTokenV2(deviceCode)
+        val prefManager = PrefManager
+        val userCookie = if (prefManager.isLogin) {
+            "uid=${prefManager.uid}; username=${prefManager.username}; token=${prefManager.token}"
+        } else {
+            SESSID
+        }
+
+        val request = originalRequest.newBuilder().apply {
+            addHeader("User-Agent", prefManager.USER_AGENT)
             addHeader("X-Requested-With", REQUEST_WITH)
-            addHeader("X-Sdk-Int", PrefManager.SDK_INT)
+            addHeader("X-Sdk-Int", prefManager.SDK_INT)
             addHeader("X-Sdk-Locale", LOCALE)
             addHeader("X-App-Id", APP_ID)
             addHeader("X-App-Token", token)
-            addHeader("X-App-Version", PrefManager.VERSION_NAME)
-            addHeader("X-App-Code", PrefManager.VERSION_CODE)
-            addHeader("X-Api-Version", PrefManager.API_VERSION)
+            addHeader("X-App-Version", prefManager.VERSION_NAME)
+            addHeader("X-App-Code", prefManager.VERSION_CODE)
+            addHeader("X-Api-Version", prefManager.API_VERSION)
             addHeader("X-App-Device", deviceCode)
-            addHeader("X-Dark-Mode", DARK_MODE)
+            addHeader("X-Dark-Mode", DARK_MODE.toString())
             addHeader("X-App-Channel", CHANNEL)
-            addHeader("X-App-Mode", MODE)
-            addHeader("X-App-Supported", PrefManager.VERSION_CODE)
+            addHeader("X-App-Mode", MODE.toString())
+            addHeader("X-App-Supported", prefManager.VERSION_CODE)
             addHeader("Content-Type", "application/x-www-form-urlencoded")
-            if (PrefManager.isLogin)
-                addHeader(
-                    "Cookie",
-                    "uid=${PrefManager.uid}; username=${PrefManager.username}; token=${PrefManager.token}"
-                )
-            else addHeader("Cookie", SESSID)
-        }
-        return chain.proceed(builder.build())
+            addHeader("Cookie", userCookie)
+        }.build()
+
+        return chain.proceed(request)
     }
 }
